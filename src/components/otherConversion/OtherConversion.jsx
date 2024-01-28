0da38860266c95bae185a4bff49180b7c5986058
unknown <ffalduti26@gmail.com>
2024-01-28 20:19:36 -0300

    arreglo final de campo

--- src/components/otherConversion/OtherConversion.jsx
+++ src/components/otherConversion/OtherConversion.jsx
@@ -11,11 +11,12 @@ export default function OtherConversion(props) {
   } = props;
 
   const newCurrency = validador(toCurrency);
+  const newSecondRate = exchangeRateSecond * value;
   const otherCurrency = validador(fromCurrency);
 
   return (
     <div className='other-conversion'>
-         <p className='small-amount'>{value} {newCurrency} = {exchangeRateSecond} {otherCurrency}</p>
+         <p className='small-amount'>{value} {newCurrency} = {newSecondRate} {otherCurrency}</p>
     </div>
   )
 }
